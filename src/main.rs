3b0f580137d67d3c8ad565373c3b4560a5842440
Hironoiri Yamamoto <mr.nikoru918@gmail.com>
2020-10-28 22:31:07 +0900

    Add cli settings

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-use crate::ctxm::view::show_contexts;
 use crate::aws::AWS;
+use crate::ctxm::view::show_contexts;
 use crate::ctxm::CTXM;
+use clap::{App, AppSettings, Arg};
 
 mod aws;
 mod ctxm;
 
 fn main() {
-    let cli = "aws";
-    let command = "list";
+    let matches = App::new("Context Manager for CLIs")
+        .version("0.0.1")
+        .author("Hironori Yamamoto <mr.nikoru918@gmail.com>")
+        .about("Mange Contexts for CLIs")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("use-context")
+                .about("controls testing features")
+                .arg(
+                    Arg::with_name("profile")
+                        .short("p")
+                        .takes_value(true)
+                        .help("profile name")
+                        .required(true),
+                ),
+        )
+        .subcommand(App::new("list-contexts").about("controls testing features"))
+        .get_matches();
 
     let aws = AWS::default();
-    let contexts = aws.list_contexts().unwrap();
-    show_contexts(&contexts);
+    match matches.subcommand() {
+        ("use-context", Some(submatches)) => {
+            let profile = submatches.value_of("profile").unwrap();
+            aws.use_context(profile).unwrap();
+        }
+        ("list-contexts", Some(_)) => {
+            let contexts = aws.list_contexts().unwrap();
+            show_contexts(&contexts);
+        }
+        _ => (),
+    }
 }
